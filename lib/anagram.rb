fa8f1a8d62b66c9b8ab71e2b8bec4e3a13d5f403
Joshua M Loran <joshua.loran@gmail.com>
2019-03-04 23:37:16 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,8 +6,13 @@ class Anagram
   end
   
   def match(word_array)
+    return_array = []
     sorted_letters = @word.split("").sort
-    
-    
+    word_array.each do |word|
+      if word.split("").sort == sorted_letters
+        return_array << word
+      end
+    end
+    return_array
   end
 end
